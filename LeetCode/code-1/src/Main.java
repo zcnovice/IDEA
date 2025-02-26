af45db7abc968b2204fc1232a3ba2e268fcd15af
zcnovice <2739650633@qq.com>
2025-02-26 11:16:28 +0800

    第9题-1

--- LeetCode/code-1/src/Main.java
+++ LeetCode/code-1/src/Main.java
@@ -3,60 +3,68 @@ import java.util.Scanner;
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
-//    给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target
-//    的那 两个 整数，并返回它们的数组下标。
-//    你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
-//    你可以按任意顺序返回答案。
+
+/*    给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，
+    并且每个节点只能存储 一位 数字。
+    请你将两个数相加，并以相同形式返回一个表示和的链表。
+    你可以假设除了数字 0 之外，这两个数都不会以 0 开头。*/
 
     //测试代码
+    //public static void main(String[] args) {
+        //链表
+
+    //}
+
+
+
+
+
+
+
+/*    给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+    回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+    例如，121 是回文，而 123 不是。*/
+
     public static void main(String[] args) {
 
-        //int [] nums = {2,3,7,11,15};
-        int target = 0;
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入数组长度：");
-        int n = sc.nextInt();
-        System.out.println("输入数组元素：");
-        int [] nums = new int[n];
-        for(int i = 0;i<n;i++){
-            int num = sc.nextInt();
-            nums[i] = num;
+        int x = 1234321;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
         }
-        System.out.println("输入目标数：");
-        target = sc.nextInt();
-
-        for(int i = 0;i< nums.length;i++)
+        //     位数为偶                   位数为奇
+        if(x == revertedNumber || x == revertedNumber / 10)
         {
-            for(int j= i+1;j< nums.length;j++)
-            {
-                int t = nums[i]+nums[j];
-                if(t==target)
-                {
-
-                }
-            }
+            System.out.println("true");
         }
     }
 
-
-    //完全体答案
     class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            int [] result = new int[2];
-            for(int i = 0;i< nums.length;i++)
+
+        //1234321
+        public boolean isPalindrome(int x) {
+
+        //   小于0有-号      最后一位不为0，应为最后一位是0则第一位也是0，不符合条件
+            if(x<0 || (x!=0 && x%10==0))
             {
-                for(int j= i+1;j< nums.length;j++)
-                {
-                    int t = nums[i]+nums[j];
-                    if(t==target)
-                    {
-                        result[0] = i;
-                        result[1] = j;
-                    }
-                }
+                return false;
             }
-            return result;
+
+            //
+            int num = 0;
+            while(x>num)
+            {
+                //每次移动一位数到num上
+                //如：第一次 x = 123432   num = 1
+                //    第二次 x = 12343   num = 12
+                num = num*10+x%10;
+                x /= 10;
+
+            }
+
+            //判断返回
+            return (x == num  || x == num/10 );
         }
     }
 
--- LeetCode/code-1/src/test/Main9.java
+++ LeetCode/code-1/src/test/Main9.java
@@ -1,2 +1,31 @@
-package test;public class Main9 {
+package test;
+
+public class Main9 {
+
+
+    class Solution {
+
+        //1234321
+        public boolean isPalindrome(int x) {
+
+            //   小于0有-号      最后一位不为0，应为最后一位是0则第一位也是0，不符合条件
+            if(x<0 || (x!=0 && x%10==0))
+            {
+                return false;
+            }
+            //
+            int num = 0;
+            while(x>num)
+            {
+                //每次移动一位数到num上
+                //如：第一次 x = 123432   num = 1
+                //    第二次 x = 12343   num = 12
+                num = num*10+x%10;
+                x /= 10;
+
+            }
+            //判断返回
+            return (x == num  || x == num/10 );
+        }
+    }
 }
