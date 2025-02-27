0e4e9a6ccdd61deb78ffbfbea383628ff28c96cd
zcnovice <2739650633@qq.com>
2025-02-27 19:33:11 +0800

    test_1

--- Java/untitled/Java20250227/src/homework/test_T.java
+++ Java/untitled/Java20250227/src/homework/test_T.java
@@ -1,2 +1,8 @@
-package homework;public class test_T {
+package homework;
+
+public class test_T {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+    }
+
 }
