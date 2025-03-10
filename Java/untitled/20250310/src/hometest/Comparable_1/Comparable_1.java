5918333ab07f786ef15752370d88115e7a951162
zcnovice <2739650633@qq.com>
2025-03-10 23:05:30 +0800

    修改--

--- Java/untitled/20250310/src/hometest/Comparable_1/Comparable_1.java
+++ Java/untitled/20250310/src/hometest/Comparable_1/Comparable_1.java
@@ -5,6 +5,8 @@ public class Comparable_1 {
         Person p1 = new Person("张三",19);
         Person p2 = new Person("李四",23);
         //比较自定义的数据类型
-        //比如比较p1与p2谁大  ，就要我自己来写
+        //比如比较p1与p2谁大  ，就要我自己来写，有两种方法
+        int b=  p1.compareTo(p2);
+        System.out.println(b);
     }
 }
